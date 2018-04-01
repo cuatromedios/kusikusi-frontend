aa0532b58dbb950260ba4f98d1809a14aa4ed78f
Ramses <ramses@cuatromedios.com>
2018-03-31 18:36:58 -0600

    We prefer 127.0.0.1:8001 as the default host and port (server is running in :8000

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export default
   version: '0.1.0',
   langs: ['en', 'es'],
   api: {
-    url: (process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8088' : '') + '/api'
+    url: (process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000' : '') + '/api'
   }
 }
