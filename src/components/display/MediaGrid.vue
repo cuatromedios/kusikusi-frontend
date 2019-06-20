80e6294b5e19e117a4e980621b5c8aabaf76d7d9
Ramses <ramses@cuatromedios.com>
2019-06-20 10:13:50 -0500

    Be able to delete media relations

--- src/components/display/MediaGrid.vue
+++ src/components/display/MediaGrid.vue
@@ -5,30 +5,39 @@
       <q-btn class="absolute-top-right q-ma-md" outline color="positive"  icon="add_circle"  :label="`${$t('general.add')} ${$t('media.singular')}`" @click="setMediumDialog('new')" />
     </q-card-section>
     <q-card-section>
-      <div class="row wrap q-col-gutter-sm q-pa-xs items-stretch">
-        <div class="media-container col-3 col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-2"
-             v-for="entity in $store.getters['content/getRelationsByKind']('medium')"
+      <div class="row wrap q-col-gutter-md q-mt-xs justify-center items-stretch">
+        <div class="col-3 col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-2" v-for="entity in $store.getters['content/getRelationsByKind']('medium')"
              :key="entity.id">
-          <q-img
-              :src="`${mediaUrl}${entity.medium.thumb}`"
-              class=""
-              v-if="['jpg', 'png', 'gif'].indexOf(entity.medium.format) !== -1"
-              basic
-              :ratio="1"
-          >
-            <template v-slot:error>
-              <div class="absolute-full flex flex-center bg-negative text-white">
-                Cannot load image
+          <q-card class="media-container">
+            <q-img
+                :src="`${mediaUrl}${entity.medium.thumb}`"
+                class=""
+                v-if="['jpg', 'png', 'gif'].indexOf(entity.medium.format) !== -1"
+                basic
+                :ratio="1"
+            >
+              <template v-slot:error>
+                <div class="absolute-full flex flex-center bg-negative text-white">
+                  Cannot load image
+                </div>
+              </template>
+            </q-img>
+            <div  v-if="['jpg', 'png', 'gif'].indexOf(entity.medium.format) === -1"
+                  class="flex flex-center bg-grey full-width text-white square">
+                <q-avatar
+                    icon="attachment"
+                    color="grey-5"
+                    text-color="white" size="64px"
+                />
+            </div>
+            <q-card-actions align="around" class="media-card-actions">
+              <div class="media-card-actions-title" style="width: 80%">
+                <span>{{ entity.medium.filename }}</span><br>
+                <span class="text-grey-5">{{ Math.round(entity.medium.size / 1024) }} Kb</span>
               </div>
-            </template>
-          </q-img>
-          <div style="background-color: azure">
-            <q-icon
-                name="attachment"
-                style="font-size: 4.4em;"
-                v-if="['jpg', 'png', 'gif'].indexOf(entity.medium.format) === -1"
-            />
-          </div>
+              <q-btn style="width: 20%" flat size="small" dense icon="link_off" color="grey" @click="deleteMedia(entity.id)"></q-btn>
+            </q-card-actions>
+          </q-card>
         </div>
       </div>
     </q-card-section>
@@ -57,10 +66,20 @@ export default {
     setMediumDialog (id) {
       this.currentMediumId = null
       this.currentMediumId = id
+    },
+    async deleteMedia (mediaId) {
+      await this.$api.delete(`/entity/${this.$store.state.content.entity.id}/relations/medium/${mediaId}`)
+      this.$router.go()
     }
   }
 }
 </script>
 
 <style lang="stylus">
+  .media-card-actions
+    height 3rem
+    .media-card-actions-title
+      overflow hidden
+      text-overflow ellipsis
+      font-size 0.9em
 </style>
